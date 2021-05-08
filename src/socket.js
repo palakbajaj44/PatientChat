0f104cbc5b253f7401c1e6cc731b2a72c293c156
Khushwant Sharma <500062206@stu.upes.ac.in>
2021-05-08 08:58:05 +0530

    cha

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,4 @@
 import { io } from 'socket.io-client';
- // const Socket= io('https://git.heroku.com/patient-connect-chat-server.git',{
   const Socket= io('https://patient-connect-chat-server.herokuapp.com/',{
       autoConnect:false
   });
